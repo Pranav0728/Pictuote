290f3b9bbb0a56f5d8700cb4e74eb53480bf2120
Pranav Molawade <pranavmolawade123@gmail.com>
2024-09-24 16:23:28 +0530

    ntc

--- src/app/api/quotes/route.js
+++ src/app/api/quotes/route.js
@@ -2,12 +2,11 @@ import { NextResponse } from "next/server";
 
 export async function GET() {
   try {
-    const response = await fetch(`https://zenquotes.io/api/quotes?random=${math.random()}`);
+    const response = await fetch(`https://zenquotes.io/api/quotes?random=${Math.random()}`);
     const data = await response.json();
 
     // Set no-cache headers
     const apiResponse = NextResponse.json(data);
-    apiResponse.headers.set('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
     return apiResponse;
   } catch (e) {
     return new NextResponse(
